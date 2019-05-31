7c8b8a62f0e94a3cb9d90e02f46aade574569f16
Andre Behrens <andre.behrens@codehub.org>
2019-05-31 10:37:21 +0200

    source code refactoring

--- lib/controller/progress_tracker.dart
+++ lib/controller/progress_tracker.dart
@@ -1,1 +1,1 @@
-class ProgressTracker {}
+import 'package:watleuk/model/translation.dart';class ProgressTracker {  Translation _currentTranslation;  void setCurrentTranslation(Translation _current) {    this._currentTranslation = _current;  }  Translation getCurrentTranslation() {    return _currentTranslation;  }}
